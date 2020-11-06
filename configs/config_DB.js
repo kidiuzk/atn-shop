977d276b28894d94f17f601c6ff706907c11bc8d
Le Duc Anh <kidiuzk@gmail.com>
2020-11-06 16:25:11 +0700

    update finish

---
+++ configs/config_DB.js
@@ -0,0 +1,16 @@
+module.exports = {
+    localdb: {
+        urldb : 'mongodb://localhost:27017/Shopacc',
+        dbname : "Shopacc",
+        dbusername : "",
+        dbpassword : "",
+    },
+
+    clouddb: {
+        urldb : 'mongodb+srv://leducanh:J5aZh13lWlSOUOxf@cluster0.uinee.mongodb.net/Shopacc?retryWrites=true&w=majority',
+        dbname : "Shopacc",
+        dbusername : "leducanh",
+        dbpassword : "J5aZh13lWlSOUOxf",
+    }
+
+};
