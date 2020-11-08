2bbfc949a197a70322bc37694cf9956e1011ef73
kidiuzk <71746860+kidiuzk@users.noreply.github.com>
2020-11-08 20:36:07 +0700

    #

--- configs/config_DB.js
+++ configs/config_DB.js
@@ -7,7 +7,7 @@ module.exports = {
     },
 
     clouddb: {
-        urldb : 'mongodb+srv://leducanh:anh123456xf@cluster0.uinee.mongodb.net/Shopacc?retryWrites=true&w=majority',
+        urldb : 'mongodb+srv://leducanh:anh123456@cluster0.uinee.mongodb.net/Shopacc?retryWrites=true&w=majority',
         dbname : "atnshop",
         dbusername : "leducanh",
         dbpassword : "anh123456",
