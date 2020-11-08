a9913328d3ffa27291e668607cfbf8d914da3f61
kidiuzk <71746860+kidiuzk@users.noreply.github.com>
2020-11-08 11:22:29 +0700

    #

--- configs/config_DB.js
+++ configs/config_DB.js
@@ -1,6 +1,6 @@
 module.exports = {
     localdb: {
-        urldb : 'mongodb+srv://leducanh:anh123456xf@cluster0.uinee.mongodb.net/Shopacc?retryWrites=true&w=majority',
+        urldb : 'mongodb+srv://leducanh:leducanh@cluster0.uinee.mongodb.net/atnshop?retryWrites=true&w=majority',
         dbname : "atnshop",
         dbusername : "leducanh",
         dbpassword : "anh123456",
